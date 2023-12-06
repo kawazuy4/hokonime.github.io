5794777e03888cfa9dffddc82462a1785df75d1e
kawazuy4 <135975260+kawazuy4@users.noreply.github.com>
2023-12-06 12:02:43 +0700

    Add files via upload

--- javascript/player.js
+++ javascript/player.js
@@ -4,13 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const params = new URLSearchParams(window.location.search);
     const film = params.get('film');
     const title = params.get('title');
+    const synopsis = params.get('synopsis');
     
     if (film) {
-        playFilm(`${film}`, title);
+        playFilm(`${film}`, title, synopsis);
     }
 });
 
-function playFilm(source, title) {
+function playFilm(source, title, synopsis) {
     const videoSource = document.getElementById('videoSource');
     videoSource.src = source;
     const videoPlayer = document.getElementById('videoPlayer');
@@ -18,5 +19,7 @@ function playFilm(source, title) {
     videoPlayer.play();
 
     const movieTitle = document.getElementById('movieTitle');
+    const movieSynopsis = document.getElementById('movieSynopsis');
     movieTitle.textContent = title;
+    movieSynopsis.textContent = synopsis
 }
